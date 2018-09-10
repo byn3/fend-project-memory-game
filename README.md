cc0f99cc1b44a06ce291b425175f210319c2a155
Bryan Leung <byn@Bryans-MacBook-Pro.local>
2018-09-09 23:33:16 -0700

    Initial commit. Added all of the Javascript to the code.
    Slightly formatted and added a tiny bit of HTML and CSS.
    Mostly added the programming parts to this game via JS.
    Imported useful tools. Code works and is responsice and interactive.

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
 # Memory Game Project
 
-## Table of Contents
+## Installation Instructions
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
+Download or clone this repository.  
+Open index.html in your web browser.  
+Enjoy!  
 
-## Instructions
+## Gameplay
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+* Flip a card. Each card flip is considered one 'move' and the number of moves will determine your star rank.  
+* The player will then flip another card and if the symbol matches, the cards will turn green.  
+* If they do not match, then the cards will flip back over.  
+* The game ends when all cards have been matched.  
+* 3 star is 20 moves or less, 2 star 38 moves or less, and 1 star is more than 38.
 
-To get started, open `js/app.js` and start building out the app's functionality
+## Requirements and dependencies
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+* A computer and an internet connection.  
 
-## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+### Contributors
 
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+Udacity provided the skeleton of this project (https://github.com/udacity/fend-project-memory-game) containing the HTML and CSS.
+I handled the Javascript section (app.js) and used jQuery, font-awesome, and sweetalert2.
+This was done as part of the Front-End Web Developer Nanodegree program which I earned a scholarship in.
+This project falls under the sections: web foundations and programming with Javascript.
+
+#### Bugs
+
+Currently matched cards may be reselected. I'm not sure if I want to allow or disallow this.
--- js/app.js
+++ js/app.js
@@ -1,19 +1,32 @@
-/*
- * Create a list that holds all of your cards
+//Begin and initialize the game via start and the global variables
+start();
+let numOfMoves = 0;
+let flipped = [];
+let timer = 0;
+stars = document.getElementsByClassName('fa-star');
+//create timer global variable which will be used later to stop timer. updates every second.
+timerFunction = setInterval(function() {
+    timer += 1;
+    $('.time').text(timer);
+}, 1000);
+
+/**
+ * @description Displays cards and shuffles list of cards shuffle the list of cards using the shuffle function below
+ * No parameters or return values. This function just loops through each card and created its HTML
  */
+function start() {
+//each of these symbols is just a font awesome icon, hence the fa- + icon concatenation in the for loop
+    const symbols = ['diamond', 'diamond', 'paper-plane-o', 'paper-plane-o', 'anchor', 'anchor', 'bolt', 'bolt', 'cube', 'cube', 'leaf', 'leaf', 'bicycle', 'bicycle', 'bomb', 'bomb']
+    let shuffled = shuffle(symbols);
+    let deck = document.getElementsByClassName('deck')[0];
+    for (let i = 0; i <= 15; i += 1){
+        deck.innerHTML += '<li class="card"><i class="fa fa-' + shuffled[i] + '"></i></li>';
+    }
+}
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
+// Shuffle function from http://stackoverflow.com/a/2450976.
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -21,10 +34,8 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +47,88 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+/**
+* This was used to help test my CODE. To use this, uncomment the HTML autowin code.
+* This was so helpful with debugging my win scenarios since to check if everything works, I had to play the game and actually win.
+$('.autowin').click(function() {
+    flipped.length = 16;
+    clearInterval(timerFunction);
+ });
+*/
+
+/**
+* @description THE MEAT OF THE CODE. If the card is clicked, do stuff.
+* Only if it hasn't been opened or if it is a match, then be interactive.
+* Add the show and open classes. Add that card to the stack. Increase #ofMoves. Update #ofMoves HTML.
+* Run the starCheck program to see if we derank from 3 to 2 stars or 2 to 1 star.
+* If we have an even number of flipped cards, run card check,
+* As well as set a 1 second timer so the user can't speed click everything.
+*/
+$('.card').click(function() {
+    if(!($(this).hasClass('open') || $(this).hasClass('match')) && $('.open').length < 2) {
+        $(this).addClass('show');
+        $(this).addClass('open');
+        flipped.push($(this));
+        numOfMoves += 1;
+        $('.numOfMoves').text(numOfMoves);
+        starCheck();
+        if (flipped.length % 2 == 0)  {
+            setTimeout(cardsCheck, 1000);
+        }
+    }
+});
+
+//check if icons match on the cards. manipulates classes to achieve css goal.
+function cardsCheck() {
+    if (flipped[flipped.length - 2].html() == flipped[flipped.length - 1].html()) {
+        flipped[flipped.length - 2].removeClass('open');
+        flipped[flipped.length - 1].removeClass('open');
+        flipped[flipped.length - 2].addClass('matched');
+        flipped[flipped.length - 1].addClass('matched');
+    } else {
+        flipped[flipped.length - 2].removeClass('open');
+        flipped[flipped.length - 2].removeClass('show');
+        flipped[flipped.length - 1].removeClass('open');
+        flipped[flipped.length - 1].removeClass('show');
+        flipped.pop(); //LIFO
+        flipped.pop();
+    }
+    //win checker, if flipped array contain 16 cards, winner winner chicken dinner.
+    if (flipped.length == 16) {
+        //set stars and stop the timer
+        stars = document.getElementsByClassName('fa-star').length;
+        clearInterval(timerFunction);
+        //call on sweetalerts2
+        swal({
+            title: 'GG! You won!',
+            text: 'It took you ' + timer + ' seconds to match all 16 cards! You got ' + stars + ' star(s) with ' + numOfMoves + ' moves.',
+            icon: 'success',
+            button: 'Replay',
+            closeOnEsc: false,
+            allowOutsideClick: false,
+        }).then(function(isConfirm) {
+            if (isConfirm) {
+                // handles Confirm button click
+                window.location.reload();
+            }
+          }).catch(swal.noop) // handles the no call but this might be depreciated...
+
+    }
+}
+
+/**
+* @description just checks if the number of moves has incremented past this range.
+* if so we decreast the stars by changing the font awesome to an empty star symbol.
+*/
+function starCheck() {
+    if (numOfMoves == 21) {
+        stars[2].classList.add('fa-star-o');
+        stars[2].classList.remove('fa-star');
+    }
+    else if (numOfMoves == 39) {
+        stars[1].classList.add('fa-star-o');
+        stars[1].classList.remove('fa-star');
+    }
+}
